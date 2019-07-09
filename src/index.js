2627ce92686ecbfdcf261d5bb51f79ff1ee4bb35
acidspud <jaqueshorn@gmail.com>
2019-07-09 13:37:33 +0200

    Initial commit
    - Add the step-promise-looper module, to be used in other projects
    
    Also in this commit
    - Use javascript standard style (https://standardjs.com/)
    - Includes tests.

---
+++ src/index.js
@@ -0,0 +1,85 @@
+
+const $internals = {
+  status: {
+    running: false,
+    stopAtEnd: false
+  },
+  steps: [],
+  loop: Promise.resolve(),
+  createStep: (index) => {
+    return (result) => {
+      const { steps, status } = $internals
+
+      if (!status.running) {
+        return Promise.reject(new Error('Loop stop forced.'))
+      }
+
+      return steps[index](result)
+    }
+  },
+  runOnce: () => {
+    let { loop } = $internals
+    const { createStep, status, steps } = $internals
+
+    status.running = true
+
+    for (let i = 0; i < steps.length; i++) {
+      loop = loop.then(createStep(i))
+    }
+
+    // Last step to indicate end of steps.
+    loop = loop.then((result) => {
+      status.running = false
+      return Promise.resolve(result)
+    })
+
+    return loop
+  },
+  runLoop: async () => {
+    const { runOnce, runLoop, status } = $internals
+
+    return runOnce().then((result) => {
+      if (status.stopAtEnd) {
+        status.running = false
+        return Promise.resolve(result)
+      }
+
+      return runLoop()
+    })
+  },
+  init: (options) => {
+    if (options) {
+      const { steps } = options
+
+      if (steps) {
+        $internals.steps = steps
+      }
+    }
+
+    return {
+      running: $internals.status.running,
+      runOnce: () => {
+        return $internals.runOnce()
+      },
+      start: async () => {
+        const { runLoop, status } = $internals
+
+        if (!status.running) {
+          return runLoop()
+        }
+      },
+      stop: () => {
+        const { status } = $internals
+
+        status.running = false
+      },
+      stopAtEnd: () => {
+        const { status } = $internals
+
+        status.stopAtEnd = true
+      }
+    }
+  }
+}
+
+module.exports = $internals.init
---
+++ tests/index.js
@@ -0,0 +1,209 @@
+import test from 'ava'
+import stepPromisedLoopModule from '../src'
+
+/* Add environmental vars */
+// import path from 'path';
+// require('dotenv').config({
+//   path: path.join(__dirname, '../config/.env')
+// });
+
+const $internals = {
+  banlist: [],
+  steps: (t) => ({
+    success: [
+      (result) => {
+        t.log('Step1: result: ', result)
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            resolve({
+              msg: 'Step1 - testData!!'
+            })
+          }, 1000)
+        })
+      },
+      (result) => {
+        t.log('Step2: result: ', result)
+
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            resolve({
+              msg: 'Step2 - testData!!'
+            })
+          }, 1000)
+        })
+      },
+      (result) => {
+        t.log('Step3: result: ', result)
+        return Promise.resolve({
+          msg: 'Step3 - testData!!'
+        })
+      }
+    ],
+    fail: [
+      (result) => {
+        t.log('Step1: result:', result)
+        return Promise.resolve({
+          msg: 'Step1 - testData!!'
+        })
+      },
+      (result) => {
+        t.log('Promise.reject following: result:', result)
+        return Promise.reject(new Error('Intentionally throwing a error in the step.'))
+      },
+      (result) => {
+        t.log('Step3', result)
+        return Promise.resolve({
+          msg: 'Step3 - testData!!'
+        })
+      }
+    ],
+    longRunning: [
+      (result) => {
+        t.log('Step1', result)
+        return Promise.resolve({
+          msg: 'Step1 - testData!!'
+        })
+      },
+      (result) => {
+        t.log('Step2: Resolve after 5 seconds.', result)
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            resolve({
+              msg: 'Step2 - testData!!'
+            })
+          }, 5000)
+        })
+      },
+      (result) => {
+        t.log('Step3: should not reach this step.', result)
+        return Promise.resolve({
+          msg: 'Step3 - testData!!'
+        })
+      }
+    ]
+  })
+}
+
+// -- Step Tests
+test('Test for the Promise Loop - start LOOP', async (t) => {
+  const { steps } = $internals
+  const { success } = steps(t)
+
+  const promiseLoop = stepPromisedLoopModule({
+    steps: success
+  })
+
+  const stopAfter3Seconds = new Promise((resolve) => {
+    setTimeout(() => {
+      t.log('Resolved the loop stop after 3 seconds.')
+      promiseLoop.stopAtEnd()
+      resolve()
+    }, 15000) // eslint-disable-line no-magic-numbers
+  })
+
+  // Check if object init correctly
+  t.log('Init - Start LOOP.')
+  t.is(typeof promiseLoop, 'object')
+
+  // Run through the steps once
+  try {
+    t.log('Start the loop.')
+    await Promise.all([promiseLoop.start(), stopAfter3Seconds])
+    // const data = await promiseLoop.start();
+  } catch (error) {
+    t.log(error)
+  }
+
+  t.log('End of loop.')
+})
+
+test('Test for the Promise Loop - Successful Steps.', async (t) => {
+  const { steps } = $internals
+  const { success } = steps(t)
+
+  const promiseLoop = stepPromisedLoopModule({
+    steps: success,
+    logger: console
+  })
+
+  // Check if object init correctly
+  t.log('Init Successful Steps.')
+  t.is(typeof promiseLoop, 'object')
+
+  // Run through the steps once
+  try {
+    t.log('Start the Steps.')
+    const data = await promiseLoop.runOnce()
+
+    t.log('Run All steps', data)
+    t.pass()
+  } catch (error) {
+    t.fail(error)
+  }
+
+  t.log('End the loop.')
+})
+
+test('Test for the Promise Loop - Failed Step.', async (t) => {
+  const { steps } = $internals
+  const { fail } = steps(t)
+
+  const promiseLoop = stepPromisedLoopModule({
+    steps: fail,
+    logger: console
+  })
+
+  // Check if object init correctly
+  t.log('Init - Failed Step.')
+  t.is(typeof promiseLoop, 'object')
+
+  // Run through the steps once
+  try {
+    t.log('Start the loop.')
+
+    const data = await promiseLoop.runOnce()
+
+    t.log('Should not reach this spot.', data)
+  } catch (error) {
+    t.log(error)
+    t.pass(error)
+  }
+
+  t.log('End of loop.')
+})
+
+test('Test for the Promise Loop - Long Running Step.', async (t) => {
+  const { steps } = $internals
+  const { longRunning } = steps(t)
+
+  const promiseLoop = stepPromisedLoopModule({
+    steps: longRunning,
+    logger: console
+  })
+
+  const stopAfter2Seconds = new Promise((resolve) => {
+    setTimeout(() => {
+      t.log('Resolved the step, stop after 2 seconds.')
+      promiseLoop.stop()
+      resolve()
+    }, 2000) // eslint-disable-line no-magic-numbers
+  })
+
+  // Check if object init correctly
+  t.log('Init - Long Running Step.')
+  t.is(typeof promiseLoop, 'object')
+
+  // Run through the steps once
+  try {
+    t.log('Start the processing steps. Then after 2 seconds, stop it.')
+    const data = await Promise.all([stopAfter2Seconds, await promiseLoop.runOnce()])
+
+    t.log('Should not reach this spot.', data)
+    t.fail()
+  } catch (error) {
+    t.log(error)
+    t.pass()
+  }
+
+  t.log('End of loop.')
+})
